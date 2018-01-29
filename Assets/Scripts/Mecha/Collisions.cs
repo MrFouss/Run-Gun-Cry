5c497106d19091c527cd723a4fbe45e9853676fa
Paul Baril <paulbaril1995@hotmail.fr>
2018-01-28 21:57:34 -0500

    Implement Damages to the mecha and collisions
    Tags for collisions : Enemy_Charger, Enemy_Laser, Void, Obstacle_Wall.
    Collisions.cs : Script to handle collisions regarding the mecha.
    MechaResources.cs : Script to access and store variables for the
    mecha : health, shield, energy. Also used to handle damages to the
    mecha.

---
+++ Assets/Scripts/Mecha/Collisions.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collisions : MonoBehaviour {
+
+    [HideInInspector]
+    protected BoxCollider boxCollider;
+    [HideInInspector]
+    protected Rigidbody rbd;
+
+	// Use this for initialization
+	void Start () {
+       boxCollider = GetComponent<BoxCollider>();
+       rbd = GetComponent<Rigidbody>(); 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+    // collision with :
+    // enemy projectiles - "enemy_laser"
+    // void (mecha falling between platforms) - "void"
+    // charging ennemies - "enemy_charger"
+    // obstacles - "obstale_wall" - "obstacle_trap"
+    void OnTriggerEnter(Collider collider)
+    {
+        Debug.Log("Trigger from" + collider.gameObject.tag);
+        switch (collider.gameObject.tag)
+        {
+            case ("Enemy_Laser"):
+                Debug.Log("shot by enemy laser");
+                MechaResources.Mecha_DamageTaken(5);
+                break;
+            case ("Void"):
+                Debug.Log("Fell in the void");
+                MechaResources.Mecha_DamageTaken(30);
+                break;
+            case ("Enemy_Charger"):
+                Debug.Log("Hit by charger");
+                MechaResources.Mecha_DamageTaken(20);
+                break;
+            case ("Obstacle_Wall"):
+                Debug.Log("Hit a wall");
+                MechaResources.Mecha_DamageTaken(30);
+                break;
+            default:
+                break;
+        }
+    }
+}
---
+++ Assets/Scripts/Mecha/MechaResources.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MechaResources : MonoBehaviour {
+
+    // resources
+    public static int health;
+    public static int shield;
+    public static int energy;
+
+    private static bool isDead;
+    private static bool isShieldEmpty;
+    private static bool isEnergyEmpty;
+
+	// Use this for initialization
+	void Start () {
+        // initial resources
+        health = 100;
+        shield = 100;
+        energy = 75;
+
+        isDead = false;
+        isShieldEmpty = false;
+        isEnergyEmpty = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(health <= 0)
+        {
+            isDead = true;
+            Debug.Log("Mecha is Destoyed");
+        }
+
+        if (shield <= 0)
+        {
+            isShieldEmpty = true;
+        }
+        else
+        {
+            isShieldEmpty = false;
+        }
+
+        if (energy <= 0)
+        {
+            isEnergyEmpty = true;
+        }
+        else
+        {
+            isEnergyEmpty = false;
+        }
+    }
+
+    public static void Mecha_DamageTaken(int damage)
+    {
+        if (isShieldEmpty)
+        {
+            health -= damage;
+        }
+        else
+        {
+            if (damage - shield > 0)
+            {
+                int remainingDamage = damage - shield;
+                shield = 0;
+                health -= remainingDamage;
+            }
+            else
+            {
+                shield -= damage;
+            }
+
+        }
+        Debug.Log("Health : " + health + "\n" + "Shield : " + shield);
+    }
+}
