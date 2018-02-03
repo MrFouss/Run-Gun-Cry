e97a63a51fa0e167cd40bb3a3a031e45c59086a6
MrFouss <maxime.brodat@fouss.fr>
2018-02-02 22:40:21 -0500

    Add obstacle scripts and demo sounds

---
+++ Assets/Scripts/Obstacles/ObstacleCollision.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleCollision : MonoBehaviour {
+
+    public BoxCollider boxCollider;
+    public ObstacleData obstacleData;
+
+    // Feedback sounds
+    public AudioClip obstacleHitSound;
+    public AudioClip obstacleDestroyedSound;
+
+    // Use this for initialization
+    void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    // Detects collisions between the obstacle and:
+    //  - the body of the mecha ("MechaBody")
+    //  - a missile launched by the mecha ("MechaMissile")
+    //  - a bullet fired by the mecha ("MechaBullet")
+    private void OnTriggerEnter(Collider other) {
+        switch (other.gameObject.tag) {
+            case Tags.MechaBodyTag:
+            case Tags.MechaMissileTag:
+                obstacleData.DestroyObstacle();
+                AudioSource.PlayClipAtPoint(obstacleDestroyedSound, transform.position);
+                break;
+            case Tags.MechaBulletTag:
+                AudioSource.PlayClipAtPoint(obstacleHitSound, transform.position);
+                break;
+        }
+    }
+}
---
+++ Assets/Scripts/Obstacles/ObstacleData.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleData : MonoBehaviour {
+
+    // Data about an obstacle
+    public int structurePoints;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    // Destroys the obstacles
+    public void DestroyObstacle() {
+        Destroy(gameObject);
+    }
+}
