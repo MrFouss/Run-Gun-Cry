fec8ce1473ccebf50628aba828bb992a65e1364c
Baneo <aurelien.durance@utbm.fr>
2018-02-05 16:39:48 -0500

    Add functional mouse controller with use of laser and missile

---
+++ Assets/Scripts/Mecha/Shots/CannonBehavior.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CannonBehavior : MonoBehaviour {
+
+	public Transform muzzle;
+	public GameObject laser_shot;
+    public GameObject missile_shot;
+    public float fire_rate_laser;
+    public float fire_rate_missile;
+
+    private float next_fire;
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+		if (Input.GetKey("mouse 0") && Time.time > next_fire)
+		{
+            next_fire = Time.time + fire_rate_laser;
+			GameObject go = GameObject.Instantiate(laser_shot, muzzle.position, muzzle.rotation) as GameObject;
+			//GameObject.Destroy(go, 3f);
+		}
+
+        if (Input.GetKey("mouse 1") && Time.time > next_fire)
+        {
+            next_fire = Time.time + fire_rate_missile;
+            GameObject go = GameObject.Instantiate(missile_shot, muzzle.position, muzzle.rotation) as GameObject;
+            //GameObject.Destroy(go, 3f);
+        }
+
+    }	
+}
---
+++ Assets/Scripts/Mecha/Shots/LaserBehavior.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LaserBehavior : MonoBehaviour {
+
+    public AudioClip shot_sound;
+    public AudioClip destroy_sound;
+
+    public int fuse_time;
+
+    AudioSource audio_source;
+
+	// Use this for initialization
+	void Start ()
+    {
+        Invoke("Explode", fuse_time);
+        audio_source = GetComponent<AudioSource>();
+        audio_source.clip = shot_sound;
+        audio_source.Play();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		transform.position += transform.forward * Time.deltaTime * 1000f;
+	
+	}
+    
+    private void OnCollisionEnter(Collision collision)
+    {
+        audio_source.Stop();
+        audio_source.PlayOneShot(destroy_sound, 1F);
+        Explode();
+    }
+
+    void Explode()
+    {
+        var exp = GetComponent<ParticleSystem>();
+        exp.Play();
+        Destroy(gameObject, 0.25f);
+    }
+
+}
