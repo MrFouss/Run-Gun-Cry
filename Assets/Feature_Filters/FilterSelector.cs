22c9c684afb6442f14a24f0a02f3176b7ca310cf
Damien Maveyraud <damienmaveyraud@hotmail.fr>
2018-02-05 22:07:27 -0500

    Add Filters system prefab and its components
    Contains a prefab used to have RGB filters
    to change the HUD and detect enemies.
    
    Scripts, sprites and test scene are also
    included.

---
+++ Assets/Feature_Filters/FilterSelector.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FilterSelector : MonoBehaviour {
+
+    private int SelectedFilter;
+    private int PreviousFilter;
+
+	// Use this for initialization
+	void Start () {
+        SelectedFilter = 0;
+        PreviousFilter = 1;
+        UpdateFilters();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        PreviousFilter = SelectedFilter;
+        // Check scrollwheel
+        if (Input.GetAxis("Mouse ScrollWheel") > 0f)
+        {
+            SelectedFilter = (SelectedFilter + 1) % 3;
+            UpdateFilters();
+        } else if (Input.GetAxis("Mouse ScrollWheel") < 0f)
+        {
+            SelectedFilter = (SelectedFilter + 2) % 3;
+            UpdateFilters();
+        }
+    }
+
+    void UpdateFilters()
+    {
+        transform.GetChild(0).gameObject.transform.GetChild(SelectedFilter).gameObject.SetActive(true);
+        transform.GetChild(0).gameObject.transform.GetChild(PreviousFilter).gameObject.SetActive(false);
+    }
+
+    public int GetSelectedFilter()
+    {
+        return SelectedFilter;
+    }
+}
