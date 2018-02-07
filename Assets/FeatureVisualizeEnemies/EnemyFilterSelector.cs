ff9db70f87210dc9b7bd88e925c34a70b9f56460
Damien Maveyraud <damienmaveyraud@hotmail.fr>
2018-02-06 23:24:58 -0500

    Add a visualizer to see specific enemies when using filters
    The EnemyVisualizer prefab must be given
    the active FiltersSystem.
    The script EnemyFilterSelector is to be
    attached to enemies for them to require a
    random filter to be seen.

---
+++ Assets/FeatureVisualizeEnemies/EnemyFilterSelector.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFilterSelector : MonoBehaviour {
+
+    // This script can be attached to enemies who require a special filter to be seen
+    // Attach it and a random filter will be attributed on Awake
+    // The EnemyVisualizer will take care of displaying the right ones
+    public int RequiredFilter;
+
+	void Awake() {
+        RequiredFilter = Random.Range(0, 3);
+        GameObject.Find("EnemyVisualizer").GetComponent<VisualizeEnemies>().updateEnemiesList();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+}
---
+++ Assets/FeatureVisualizeEnemies/VisualizeEnemies.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VisualizeEnemies : MonoBehaviour {
+
+    private GameObject[] enemiesList;
+    public GameObject filtersSystem;
+    private FilterSelector filterSelector;
+
+	// Use this for initialization
+	void Start () {
+        filterSelector = filtersSystem.GetComponent<FilterSelector>();
+        updateEnemiesList();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		foreach (GameObject enemy in enemiesList)
+        {
+            if (filterSelector.GetSelectedFilter() == enemy.GetComponent<EnemyFilterSelector>().RequiredFilter)
+            {
+                enemy.GetComponent<Renderer>().enabled = true;
+            } else
+            {
+                enemy.GetComponent<Renderer>().enabled = false;
+            }
+        }
+	}
+
+    // To be called whenever an enemy is created for it to be analyzed later on by the visualizer
+    public void updateEnemiesList()
+    {
+        enemiesList = GameObject.FindGameObjectsWithTag("Enemy");
+    }
+}
