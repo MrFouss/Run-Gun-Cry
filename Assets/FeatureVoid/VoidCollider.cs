f1b117ee86aacfcc084fef0c23aa1c48190585ad
Damien Maveyraud <damienmaveyraud@hotmail.fr>
2018-02-07 19:32:30 -0500

    Add components for the feature regarding the void
    When falling into the void, the mecha
    gets back on the last platform it landed on.
    PlatformMemorizer must be attached to the
    Mecha GameObject.
    VoidMechaFollower must be given the Mecha
    GameObject.
    An extra tag "Platform" is necessary for
    them to be memorized and therefore for the
    feature to work properly.

---
+++ Assets/FeatureVoid/VoidCollider.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VoidCollider : MonoBehaviour {
+
+    public int heightRespawn;
+    // This script repositions the mecha whenever he falls into the void
+    // A PlatforMemorizer Script must be attached to the mecha beforehand
+
+    void OnCollisionEnter(Collision collision)
+    {
+        GameObject other = collision.collider.gameObject;
+        if (other.tag == "MechaBody")
+        {
+            // When the Mecha falls, reposition it over the last platform it landed on
+            GameObject lastPlatform = collision.collider.gameObject.GetComponent<PlatformMemorizer>().getLastPlatform();
+            other.transform.position = new Vector3(lastPlatform.transform.position.x, lastPlatform.transform.position.y + 2 + heightRespawn, lastPlatform.transform.position.z);
+        }
+    }
+    
+}
---
+++ Assets/FeatureVoid/VoidMechaFollower.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VoidMechaFollower : MonoBehaviour {
+
+    public GameObject mecha;
+	
+	// Update is called once per frame
+	void Update () {
+        // The void collider stays under the mecha and follows the player
+        // along the z and x axis.
+        transform.position = new Vector3(mecha.transform.position.x, transform.position.y, mecha.transform.position.z);
+	}
+
+}
