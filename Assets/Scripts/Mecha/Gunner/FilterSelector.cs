049fae2c5c8f5ed75206e5ad077df026b5e75182
MrFouss <maxime.brodat@fouss.fr>
2018-02-14 14:11:37 -0500

    Fix scripts' syntax

--- Assets/Scripts/Mecha/Gunner/FilterSelector.cs
+++ Assets/Scripts/Mecha/Gunner/FilterSelector.cs
@@ -4,39 +4,39 @@ using UnityEngine;
 
 public class FilterSelector : MonoBehaviour {
 
-    private int SelectedFilter;
-    private int PreviousFilter;
+    private int selectedFilter;
+    private int previousFilter;
 
 	// Use this for initialization
 	void Start () {
-        SelectedFilter = 0;
-        PreviousFilter = 1;
+        selectedFilter = 0;
+        previousFilter = 1;
         UpdateFilters();
     }
 	
 	// Update is called once per frame
 	void Update () {
-        PreviousFilter = SelectedFilter;
+        previousFilter = selectedFilter;
         // Check scrollwheel
         if (Input.GetAxis("Mouse ScrollWheel") > 0f)
         {
-            SelectedFilter = (SelectedFilter + 1) % 3;
+            selectedFilter = (selectedFilter + 1) % 3;
             UpdateFilters();
         } else if (Input.GetAxis("Mouse ScrollWheel") < 0f)
         {
-            SelectedFilter = (SelectedFilter + 2) % 3;
+            selectedFilter = (selectedFilter + 2) % 3;
             UpdateFilters();
         }
     }
 
     void UpdateFilters()
     {
-        transform.GetChild(0).gameObject.transform.GetChild(SelectedFilter).gameObject.SetActive(true);
-        transform.GetChild(0).gameObject.transform.GetChild(PreviousFilter).gameObject.SetActive(false);
+        transform.GetChild(0).gameObject.transform.GetChild(selectedFilter).gameObject.SetActive(true);
+        transform.GetChild(0).gameObject.transform.GetChild(previousFilter).gameObject.SetActive(false);
     }
 
     public int GetSelectedFilter()
     {
-        return SelectedFilter;
+        return selectedFilter;
     }
 }
