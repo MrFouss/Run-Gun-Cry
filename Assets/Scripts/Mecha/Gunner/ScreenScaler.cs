c9f083c86a008caf55d7635825eac0f5aa951d07
MrFouss <maxime.brodat@fouss.fr>
2018-02-18 19:32:40 -0500

    Fix errors from previous commit

---
+++ Assets/Scripts/Mecha/Gunner/ScreenScaler.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScreenScaler : MonoBehaviour
+{
+
+    // Script used to scale a RawImage to fit the screen
+    // Use this for initialization
+    void Start()
+    {
+        GetComponent<RectTransform>().sizeDelta = new Vector2(Screen.width, Screen.height);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
