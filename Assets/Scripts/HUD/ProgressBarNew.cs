e0e05d240bdf5d2c1cd4cea09a9ad6ae68f4dee7
Ezia <esia.belbachir@protonmail.com>
2018-03-07 15:01:03 -0500

    Write a new progress bar script and use it in HUD scene

--- Assets/Scripts/HUD/ProgressBarNew.cs
+++ Assets/Scripts/HUD/ProgressBarNew.cs
@@ -3,27 +3,73 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+[ExecuteInEditMode, System.Serializable]
 public class ProgressBarNew : MonoBehaviour {
-    private Renderer _renderer;
-    private MaterialPropertyBlock _propBlock;
 
-    public float Progress = 0;
-
-    // Use this for initialization
-    void Start () {
-        //_propBlock = new MaterialPropertyBlock();
-    }
-	
+    public enum Direction {LEFT, RIGHT, UP, DOWN};
     
+    public Direction direction = Direction.LEFT;
+    public float ProgressPerSecond = 1;
+    public float Progress = 1;
+    [SerializeField]
+    private float ActualProgress = 1;
 
-	// Update is called once per frame
-	void Update () {
-        //// Get the current value of the material properties in the renderer.
-        //_renderer.GetPropertyBlock(_propBlock);
-        //// Assign our new value.
+    public Sprite BarMask;
+    public Color BarColor;
+
+    [SerializeField]
+    private RectTransform ProgressMask;
+    [SerializeField]
+    private RectTransform ProgressBar;
+    [SerializeField]
+    private Image BackgroundImage;
+    [SerializeField]
+    private Image BarImage;
+
+    private void Update()
+    {
+        // force sprite in editor
+        BackgroundImage.sprite = BarMask;
+        BarImage.sprite = BarMask;
+        BarImage.color = BarColor;
         
-        //// Apply the edited values to the renderer.
-        //_renderer.SetPropertyBlock(_propBlock);
+        // update actual progress
+        if (Application.isEditor)
+        {
+            ActualProgress = Progress;
+        } else
+        {
+            ActualProgress += Mathf.Clamp(Progress - ActualProgress, -Time.deltaTime * ProgressPerSecond, Time.deltaTime * ProgressPerSecond);
+        }
+
+        // reset progress bar to maximum size
+        ProgressMask.offsetMax = Vector2.zero;
+        ProgressMask.offsetMin = Vector2.zero;
+        ProgressBar.offsetMin = Vector2.zero;
+        ProgressBar.offsetMax = Vector2.zero;
+
+        // get max bar size
+        Vector2 size = ProgressMask.rect.size;
+
+        // update offsets
+        switch (direction)
+        {
+            case Direction.LEFT:
+                ProgressMask.offsetMin = new Vector2(size.x * (1 - ActualProgress), 0);
+                ProgressBar.offsetMin = new Vector2(-size.x * (1 - ActualProgress), 0);
+                break;
+            case Direction.RIGHT:
+                ProgressMask.offsetMax = new Vector2(-size.x * (1 - ActualProgress), 0);
+                ProgressBar.offsetMax = new Vector2(size.x * (1 - ActualProgress), 0);
+                break;
+            case Direction.UP:
+                ProgressMask.offsetMax = new Vector2(0, -size.y * (1 - ActualProgress));
+                ProgressBar.offsetMax = new Vector2(0, size.y * (1 - ActualProgress));
+                break;
+            case Direction.DOWN:
+                ProgressMask.offsetMin = new Vector2(0, size.y * (1 - ActualProgress));
+                ProgressBar.offsetMin = new Vector2(0, -size.y * (1 - ActualProgress));
+                break;
+        }
     }
-    
 }
