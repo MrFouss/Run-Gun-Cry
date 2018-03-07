9d3363975908fa2b2ab18e57638703805d6a3246
Ezia <esia.belbachir@protonmail.com>
2018-03-07 00:26:58 -0500

    Setup full HUD in a scene

---
+++ Assets/Scripts/HUD/ProgressBarNew.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ProgressBarNew : MonoBehaviour {
+    private Renderer _renderer;
+    private MaterialPropertyBlock _propBlock;
+
+    public float Progress = 0;
+
+    // Use this for initialization
+    void Start () {
+        //_propBlock = new MaterialPropertyBlock();
+    }
+	
+    
+
+	// Update is called once per frame
+	void Update () {
+        //// Get the current value of the material properties in the renderer.
+        //_renderer.GetPropertyBlock(_propBlock);
+        //// Assign our new value.
+        
+        //// Apply the edited values to the renderer.
+        //_renderer.SetPropertyBlock(_propBlock);
+    }
+    
+}
