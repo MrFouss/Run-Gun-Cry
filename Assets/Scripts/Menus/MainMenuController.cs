4b2b2b2298b15003131cfcc9aa633cdbda562bf3
Baneo <aurelien.durance@utbm.fr>
2018-03-07 17:30:39 -0500

    Add the highscore scene, the music for the main menu and the content of events for the main menu buttons

--- Assets/Scripts/Menus/MainMenuController.cs
+++ Assets/Scripts/Menus/MainMenuController.cs
@@ -1,21 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class MainMenuController : MonoBehaviour {
 
+    public AudioSource AudioSource;
+
+
     public void OnStartGamePress()
     {
-
+        SceneManager.LoadScene("TestScene");    
     }
 
     public void OnHighScorePress()
     {
-
+        SceneManager.LoadScene("HighScores");
     }
 
     public void OnQuitGamePress()
     {
-
+        Application.Quit();
     }
 }
