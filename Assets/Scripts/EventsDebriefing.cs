82ba222abfab55301c1184a0ac57a1bf839028ce
Baneo <aurelien.durance@utbm.fr>
2018-03-07 17:43:15 -0500

    Add the Events for the debriefing menu and add the highscore and main menu scenes to the build settings

---
+++ Assets/Scripts/EventsDebriefing.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class EventsDebriefing : MonoBehaviour {
+
+    public void OnMainMenuPress()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
