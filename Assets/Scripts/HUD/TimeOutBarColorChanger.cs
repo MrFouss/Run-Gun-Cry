4d88bb65eda160c209a13e5e6d074a6ddef95523
Ezia <esia.belbachir@protonmail.com>
2018-04-11 10:38:22 -0400

    Change progress bar color to red when arrow wrongly typed

---
+++ Assets/Scripts/HUD/TimeOutBarColorChanger.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeOutBarColorChanger : MonoBehaviour {
+
+    private ProgressBar _progressBar;
+    
+    public Color SuccessColor;
+    public Color FailureColor;
+
+    // Use this for initialization
+    void Start () {
+        _progressBar = GetComponent<ProgressBar>();
+	}
+	
+	public void SetProgressBarColor(bool success)
+    {
+        if (success)
+        {
+            _progressBar.BarColor = SuccessColor;
+        }
+        else
+        {
+            _progressBar.BarColor = FailureColor;
+        }
+    }
+}
